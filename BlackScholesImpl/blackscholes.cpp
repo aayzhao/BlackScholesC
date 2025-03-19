8552b5d15470c1a8edbc6af4073fa5473ab6bbfd
Aaroz <aaron.y.zhao1@gmail.com>
2025-03-19 12:33:46 -0400

    Add std normal CDF and PDF functions

--- BlackScholesImpl/blackscholes.cpp
+++ BlackScholesImpl/blackscholes.cpp
@@ -1,1 +1,11 @@
 #include "blackscholes.h"
+#include <cmath>
+
+double normalPDF(double x) {
+    static const double INV_SQRT_2PI = 0.3989422804014327;
+    return INV_SQRT_2PI * std::exp(-0.5 * x * x);
+}
+
+double normalCDF(double x) {
+    return 0.5 * (1.0 + std::erf(x / std::sqrt(2.0)));
+}
--- BlackScholesImpl/blackscholes.h
+++ BlackScholesImpl/blackscholes.h
@@ -1,1 +1,21 @@
 #pragma once
+
+/*
+Standard Normal Probability Density Function implementation
+*/
+double normalPDF(double x);
+
+/*
+Standard Normal Cumulative Distribution Function using the error function (erf)
+*/
+double normalCDF(double x);
+
+/*
+Computes d1 in the Black–Scholes model:
+		   ln(S/K) + (r + sigma^2 / 2) * T
+	d1 =  ---------------------------------
+	              sigma * sqrt(T)
+or: 
+	d1 = [ln(S/K) + (r + sigma^2 / 2) * T] / [sigma * sqrt(T)]
+*/
+double computeD1(double S, double K, double r, double sigma, double T);
